87552c5c5a029c1dfc952456d5dc19e88be2a364
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-10-13 09:18:59 +0800

    Create Game.java

---
+++ src/com/jayvee/game/Game.java
@@ -0,0 +1,114 @@
+package com.jayvee.game;
+
+public class Game {
+	package com.game.main;
+
+	import java.awt.Canvas;
+	import java.awt.Color;
+	import java.awt.Graphics;
+	import java.awt.image.BufferStrategy;
+	import java.util.Random;
+
+	public class Game extends Canvas implements Runnable{
+		
+		private static final long serialVersionUID = -473349850293143017L;
+
+		public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+		
+		private Thread thread;
+		private boolean running = false;
+		private Handler handler;
+		private Random r;
+		
+		public Game() {
+			new Window(WIDTH, HEIGHT, "Lets build a Game!", this);
+			handler = new Handler();
+			r = new Random();
+			int x, y, velX, velY;
+			for(int i = 0; i < 100; i++) {
+				x = r.nextInt(WIDTH);
+				y = r.nextInt(HEIGHT);
+				velX = r.nextInt(2);
+				velY = r.nextInt(2);
+				Player player = new Player(x, y, ID.Player);
+				player.setVelX(velX);
+				player.setVelY(velY);
+				
+				handler.addObject(player);
+			}
+			
+		}
+		public synchronized void start() {
+			thread = new Thread(this);
+			thread.start();
+			running = true;
+		}
+		
+		public synchronized void stop() {
+			try {
+				thread.join();
+				running = false;
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		public void run() {
+			// the game loop
+			long lastTime = System.nanoTime();
+			double amountOfTicks = 60.0;
+			double ns = 1000000000 / amountOfTicks;
+			double delta = 0;
+			long timer = System.currentTimeMillis();
+			int frames = 0;
+			while(running) {
+				long now = System.nanoTime();
+				delta += (now - lastTime) / ns;
+				lastTime = now;
+				while(delta >= 1) {
+					tick();
+					delta--;
+				}
+				if(running)
+					render();
+				frames++;
+				
+				if(System.currentTimeMillis() - timer > 1000) {
+					timer += 1000;
+					System.out.println("FPS: " + frames);
+					frames = 0;
+				}
+				
+			}
+			stop();
+			
+		}
+		
+		private void tick() {
+			handler.tick();
+		}
+		
+		private void render() {
+			BufferStrategy bs = this.getBufferStrategy();
+			if(bs == null) {
+				this.createBufferStrategy(3);
+				return;
+			}
+			
+			Graphics g = bs.getDrawGraphics();
+			
+			g.setColor(Color.black);
+			g.fillRect(0, 0, WIDTH, HEIGHT);
+			
+			handler.render(g);
+			
+			g.dispose();
+			bs.show();
+		}
+		
+		public static void main(String args[]) {
+			Game game = new Game();
+		}
+	}
+
+}
