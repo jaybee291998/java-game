e853732fc6637783aea7c613607d3bc43636d0b6
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2021-10-16 10:23:28 +0800

    prevent player from going out of bounds
    prevent the player from moving past the screen boundary

--- src/com/jayvee/game/Player.java
+++ src/com/jayvee/game/Player.java
@@ -12,6 +12,9 @@ public class Player extends GameObject{
 	public void tick() {
 		x += velX;
 		y += velY;
+		// allow the player to move if its not out of bounds
+		if(x <= 0 || x >= Game.WIDTH - 40)  x -= velX;
+		if(y <= 0 || y >= Game.HEIGHT - 65) y -= velY;
 	}
 	public void render(Graphics g) {
 		g.setColor(Color.white);
